703dac2ea5751605a7fb01d643283f6cae0299b6
Jorikos <26204@ma-web.nl>
2018-11-09 09:12:35 +0100

    Added some ascii art and array system

--- src/Main.java
+++ src/Main.java
@@ -49,16 +49,20 @@ public class Main {
                 lc2 = antwoord.toLowerCase();
                 if (lc2.equals("ja")) {
                     System.out.println("Warm of koud");
+                    antwoordenArray[0] = "ja";
                     vraag = false;
                 } else if (lc2.equals("nee")) {
                     System.out.println("Nu sluit het programma af");
                     System.exit(1);
+                    antwoordenArray[0] = "nee";
                     vraag = false;
                 } else if (lc2.equals("exit")) {
                     System.exit(1);
+                    antwoordenArray[0] = "exit";
                     vraag = false;
                 } else if (lc2.equals("saldo")) {
                     System.out.println("Je hebt nog €" + G);
+                    antwoordenArray[1] = "saldo";
                 }
             }
 
@@ -74,10 +78,12 @@ public class Main {
                         "2. Pepsi €2\n" +
                         "3. Fanta €1\n" +
                         "4. Water €0.50");
+                antwoordenArray[2] = "koud";
                 int cpfw = s.nextInt();
                 if (cpfw == 1) {
                     System.out.println("Dat word dan €2");
                     G = G - 2;
+                    antwoordenArray[3] = "Cola";
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
                         G = G + 2;
@@ -85,9 +91,9 @@ public class Main {
                     }
                     vraaag = false;
                 } else if (cpfw == 2) {
-
                     System.out.println("Dat word dan €2");
                     G = G - 2;
+                    antwoordenArray[3] = "Pepsi";
                     System.out.println("Je hebt nog €" + G);
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
@@ -98,6 +104,7 @@ public class Main {
                     vraaag = false;
                 } else if (cpfw == 3) {
                     System.out.println("Dat word dan €1");
+                    antwoordenArray[3] = "Fanta";
                     G = G - 1;
                     System.out.println("Je hebt nog €" + G);
                     if (G < 0) {
@@ -110,6 +117,7 @@ public class Main {
                 } else if (cpfw == 4) {
                     System.out.println("Dat word dan €0.50");
                     G = G - 0.5;
+                    antwoordenArray[3] = "Water";
                     System.out.println("Je hebt nog €" + G);
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
@@ -130,11 +138,27 @@ public class Main {
                         "4. Thee €1\n");
                 System.out.println("Ik wil nummer: ");
                 int kwct = s.nextInt();
+                antwoordenArray[2] = "warm";
 
                 if (kwct == 1) {
                     System.out.println("Dat word dan €2");
                     G = G - 2;
+                    antwoordenArray[3] = "Koffie";
                     System.out.println("Je hebt nog €" + G);
+                    System.out.println("         {\n" +
+                            "      {   }\n" +
+                            "       }_{ __{\n" +
+                            "    .-{   }   }-.\n" +
+                            "   (   }     {   )\n" +
+                            "   |`-.._____..-'|\n" +
+                            "   |             ;--.\n" +
+                            "   |            (__  \\\n" +
+                            "   |             | )  )\n" +
+                            "   |             |/  /\n" +
+                            "   |             /  / \n" +
+                            "   |            (  /\n" +
+                            "   \\             y'\n" +
+                            "    `-.._____..-'");
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
                         G = G + 2;
@@ -144,7 +168,14 @@ public class Main {
                 } else if (kwct == 2) {
                     System.out.println("Dat word dan €1");
                     G = G - 1;
+                    antwoordenArray[3] = "Water";
                     System.out.println("Je hebt nog €" + G);
+                    System.out.println(" mmm\n" +
+                            " )-(\n" +
+                            "(   )\n" +
+                            "|   |\n" +
+                            "|   |\n" +
+                            "|___|");
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
                         G = G + 1;
@@ -153,6 +184,7 @@ public class Main {
                 } else if (kwct == 3) {
                     System.out.println("Dat word dan €2");
                     G = G - 2;
+                    antwoordenArray[3] = "Chocolademelk";
                     System.out.println("Je hebt nog €" + G);
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
@@ -162,6 +194,7 @@ public class Main {
                 } else if (kwct == 4) {
                     System.out.println("Dat word dan €1");
                     G = G - 1;
+                    antwoordenArray[3] = "Thee";
                     System.out.println("Je hebt nog €" + G);
                     if (G < 0) {
                         System.out.println("Je hebt niet genoeg geld");
@@ -170,6 +203,7 @@ public class Main {
                     }
                 }
             }
+            System.out.println("Jouw keuzes waren: " + antwoordenArray[0] + " " + antwoordenArray[3] + " " + antwoordenArray[2] + " " + antwoordenArray[1]);
             run = 1;
         }
     }
